1fe0bd76d2baa95fb2efe3c7cd3b2cbea08209c8
bruce <bruce60108010204@gmail.com>
2023-01-19 16:10:03 +0800

    zsh

--- build_zsh.sh
+++ build_zsh.sh
@@ -1,4 +1,5 @@
-cp /opt/nvidia/deepstream/deepstream-6.1/commonFolder/.zshrc ~/
+# cp /opt/nvidia/deepstream/deepstream-6.1/commonFolder/.zshrc ~/
+cp .zshrc ~/
 zsh
 source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 source ~/.zshrc
